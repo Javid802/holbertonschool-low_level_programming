2e1f25b2bbf18fa8efe2f2262764fba68d4f3400
javid802 <gccavid123@gmail.com>
2025-12-01 17:27:19 +0400

    python

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -2,26 +2,25 @@
 #include <time.h>
 #include <stdio.h>
 
-/* betty style doc for function main goes here */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-    int n;
+	int n;
 
-    srand(time(0));
-    n = rand() - RAND_MAX / 2;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
 
-    if (n > 0)
-    {
-        printf("is positive\n");   /* \n əlavə olundu */
-    }
-    else if (n == 0)
-    {
-        printf("is zero\n");       /* \n əlavə olundu */
-    }
-    else
-    {
-        printf("is negative\n");   /* \n əlavə olundu */
-    }
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
+	else
+		printf("%d is negative\n", n);
 
-    return 0;
+	return (0);
 }
