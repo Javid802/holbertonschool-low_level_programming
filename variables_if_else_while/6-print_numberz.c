a2a44bd4ca7e56aebee1fbbf8289d29b24da40d2
javid802 <gccavid123@gmail.com>
2025-12-01 18:58:56 +0400

    python

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers from 0 to 9
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int letter1 = 0;
+
+	while (letter1 <= 9)
+	{
+		putchar(letter1 + '0');
+		letter1++;
+	}
+
+	putchar("\n");
+
+	return (0);
+}
