13c4d7c558f17882fb72a2a61a456ae072022435
javid802 <gccavid123@gmail.com>
2025-12-01 18:29:58 +0400

    python

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter1 = 'a';
+    char letter2 = 'A';
+
+	while (letter1 <= 'z')
+	{
+		printf("%c", letter1);
+		letter1++;
+	}
+    while (letter2 <= 'Z')
+	{
+		printf("%c", letter2);
+		letter2++;
+	}
+	printf("\n");
+
+	return (0);
+}
