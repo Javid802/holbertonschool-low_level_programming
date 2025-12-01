63a43d02b97138e7477e2870c3cc61c06599a56c
javid802 <gccavid123@gmail.com>
2025-12-01 17:35:45 +0400

    python

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -22,15 +22,15 @@ int main(void)
 
 	if (last_digit > 5)
 	{
-		printf(" and is greater than 5\n");
+		printf(" and is greater than 5");
 	}
 	else if (last_digit == 0)
 	{
-		printf(" and is 0\n");
+		printf(" and is 0");
 	}
 	else
 	{
-		printf(" and is less than 6 and not 0\n");
+		printf(" and is less than 6 and not 0");
 	}
 
 	return (0);
