4c8901fed704c56501441cdd0a979f9616fc123a
javid802 <gccavid123@gmail.com>
2025-12-01 18:50:54 +0400

    python

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -11,7 +11,7 @@ int main(void)
 
 	while (letter1 <= 'z')
 	{
-        if (letter1!='q'|| letter1 != 'e')
+        if (letter1!='q'&& letter1 != 'e')
         {
             printf("%c", letter1);
         }
