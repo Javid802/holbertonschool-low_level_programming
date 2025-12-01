46508487f73f457cc8095e9eb9070ab765ab417c
javid802 <gccavid123@gmail.com>
2025-12-01 19:11:25 +0400

    python

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -1,27 +1,28 @@
 #include <stdio.h>
 
 /**
- * main - Prints numbers from 0 to 9
+ * main - Prints numbers of base 16 in lowercase
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int letter1 = 0;
-    char letter2 = 'a';
+	char letter2 = 'a';
 
 	while (letter1 <= 9)
 	{
 		putchar(letter1 + '0');
 		letter1++;
 	}
-    while (letter2 <= 'f')
+
+	while (letter2 <= 'f')
 	{
 		putchar(letter2);
 		letter2++;
 	}
 
-	printf("\n");
+	putchar('\n');
 
 	return (0);
 }
