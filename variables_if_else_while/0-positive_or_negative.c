0c67ad89d41c505b444bb686ac83947b965f40f5
javid802 <gccavid123@gmail.com>
2025-12-01 17:24:50 +0400

    python

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -2,7 +2,7 @@
 #include <time.h>
 #include <stdio.h>
 
-/* betty style doc for function main goes there */
+/* betty style doc for function main goes here */
 int main(void)
 {
     int n;
@@ -12,15 +12,15 @@ int main(void)
 
     if (n > 0)
     {
-        printf("is positive");   // <-- \n əlavə olundu
+        printf("is positive\n");   /* \n əlavə olundu */
     }
     else if (n == 0)
     {
-        printf("is zero");       // <-- \n əlavə olundu
+        printf("is zero\n");       /* \n əlavə olundu */
     }
     else
     {
-        printf("is negative");   // <-- \n əlavə olundu
+        printf("is negative\n");   /* \n əlavə olundu */
     }
 
     return 0;
