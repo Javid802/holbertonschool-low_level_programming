f8fc0b608128a35ab229aaf9c68fce205f1a639d
javid802 <gccavid123@gmail.com>
2025-12-01 17:45:49 +0400

    python

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -7,30 +7,22 @@
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	int n, last_digit;
+	int n;
+	int last_digit;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-
 	last_digit = n % 10;
-	
-
-	printf("Last digit of %d is %d", n, last_digit);
-
-	if (last_digit > 5)
-	{
-		printf(" and is greater than 5");
-	}
-	else if (last_digit == 0)
-	{
-		printf(" and is 0");
-	}
-	else
-	{
-		printf(" and is less than 6 and not 0");
-	}
+	if (n % 10 > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
+	else if (n % 10 == 0)
+		printf("Last digit of %d is %d and is 0\n", n, last_digit);
+	else if ((n % 10 < 6) && (n % 10 != 0))
+		printf("Last digit of %d is %d and is less than 6 and not 0\n",
+			 n, last_digit);
 
 	return (0);
 }
