05ccc3766f845099a051a547b6da4d7dac845089
javid802 <gccavid123@gmail.com>
2025-12-02 17:38:37 +0400

    python

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of a number
+ * @n: the number to check
+ *
+ * Return: 1 if n > 0, 0 if n == 0, -1 if n < 0
+ */
+int _abs(int)
+{
+    int n;
+    if (n<0)
+    {
+        return -n;
+    }
+    return n;
+
+}
