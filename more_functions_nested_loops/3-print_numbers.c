ec2a3539bc837c392fd15fd9fd50b0c444a77d43
javid802 <gccavid123@gmail.com>
2025-12-02 19:32:15 +0400

    python

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - adds two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: sum of a and b
+ */
+void print_numbers(void)
+{
+    int a;
+    for (a=0;a<10;a++)
+    {
+        _putchar(a+'o\n');
+    }
+}
