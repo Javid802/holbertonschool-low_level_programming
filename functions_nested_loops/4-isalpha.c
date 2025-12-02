bac24a6d1f747cded7106f81a2164cdf8249c051
javid802 <gccavid123@gmail.com>
2025-12-02 16:50:36 +0400

    python

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * _islower - checks for lowercase character
- * : the character to check
+ * _isalpha - checks for alphabetic character
+ * @c: the character to check
  *
- * Return: 1 if c is lowercase, 0 otherwise
+ * Return: 1 if c is a letter (lowercase or uppercase), 0 otherwise
  */
-int _islower(int c)
+int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return (1);
