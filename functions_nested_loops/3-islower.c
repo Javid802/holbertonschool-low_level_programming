6bf9a118a6a1fd26a3266d8bab78907e451f7a96
javid802 <gccavid123@gmail.com>
2025-12-02 16:44:54 +0400

    python

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,20 +1,14 @@
 #include "main.h"
 
 /**
- * main - check the code.
+ * _islower - checks for lowercase character
+ * : the character to check
  *
- * Return: Always 0.
+ * Return: 1 if c is lowercase, 0 otherwise
  */
-int main(void)
+int _islower(int c)
 {
-    int r;
-
-    r = _islower('H');
-    _putchar(r + '0');
-    r = _islower('o');
-    _putchar(r + '0');
-    r = _islower(108);
-    _putchar(r + '0');
-    _putchar('\n');
-    return (0);
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	return (0);
 }
