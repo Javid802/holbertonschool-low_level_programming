ae1b62311c56090a2b83bc53d7767c8615ade90e
javid802 <gccavid123@gmail.com>
2025-12-02 16:33:28 +0400

    python

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,21 +1,23 @@
 #include "main.h"
 
 /**
- * print_alphabet - prints the alphabet in lowercase
+ * print_alphabet_x10 - prints the alphabet in lowercase 10 times
  */
 void print_alphabet_x10(void)
 {
 	int a = 1;
-    while (a<=10)
-    {
-        char c = 'a';
 
-	    while (c <= 'z')
-	    {
-		    _putchar(c);
-		    c++;
-	    }
-        _putchar('\n');
-        a++;
-    }    
+	while (a <= 10)
+	{
+		char c = 'a';
+
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+
+		_putchar('\n');
+		a++;
+	}
 }
