682e053455e53e1c0348c0d16525845808f6ffed
javid802 <gccavid123@gmail.com>
2025-12-02 19:27:32 +0400

    python

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _isupper - checks for uppercase character
+ * _isdigit - checks for uppercase character
  * @c: the character to check
  *
  * Return: 1 if uppercase, 0 otherwise
