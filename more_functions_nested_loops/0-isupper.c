708c966d1f989c9b2af1569887f4389549ee7553
javid802 <gccavid123@gmail.com>
2025-12-02 19:19:14 +0400

    python

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,15 +1,16 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * print_to_98 - prints all natural numbers from n to 98
- * @n: starting number
+ * _isupper - checks for uppercase character
+ * @c: the character to check
+ *
+ * Return: 1 if uppercase, 0 otherwise
  */
 int _isupper(int c)
 {
-    if (c >= 'A' && c <= 'Z')
-    {
-        return (1);
-    }
-    return (0);
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	return (0);
 }
