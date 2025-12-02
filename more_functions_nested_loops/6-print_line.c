0a6e16c739d7e6fea5be7285d09b032d7b535553
javid802 <gccavid123@gmail.com>
2025-12-03 01:05:44 +0400

    python

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,14 @@
+
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers 0 to 14, 10 times
+ */
+void print_line(int n)
+{
+	int count = 0;
+    for (count ; count <= n ; count++)
+    {
+        _putchar('_\n');
+    }
+}
