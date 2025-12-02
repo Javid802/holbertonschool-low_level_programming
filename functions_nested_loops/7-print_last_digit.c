fd4e15ad6a3bb79dd00af3de83812c41062d1c44
javid802 <gccavid123@gmail.com>
2025-12-02 17:52:12 +0400

    python

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @n: the integer to compute
+ *
+ * Return: absolute value of n
+ */
+int print_last_digit(int n)
+{
+    return n%10;
+}
