9b5b7a371b1c4fc3db7cfe7b50c3689a71f99d37
javid802 <gccavid123@gmail.com>
2025-12-02 19:14:52 +0400

    python

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - prints all natural numbers from n to 98
+ * @n: starting number
+ */
+int _isupper(int c)
+{
+    if (c > "A" && c<"Z")
+    {
+        return 1;
+    }
+    return 0;
+}
