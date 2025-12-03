8c67c1999fbeb8b427fe8541f244f0704db48c17
javid802 <gccavid123@gmail.com>
2025-12-03 14:39:45 +0400

    python

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -2,31 +2,31 @@
 
 int main(void)
 {
-    int n;
+	int n;
 
-    for (n = 1; n <= 100; n++)
-    {
-        if ((n % 3 == 0) && (n % 5 == 0))
-        {
-            printf("FizzBuzz");
-        }
-        else if (n % 3 == 0)
-        {
-            printf("Fizz");
-        }
-        else if (n % 5 == 0)
-        {
-            printf("Buzz");
-        }
-        else
-        {
-            printf("%d", n);
-        }
+	for (n = 1; n <= 100; n++)
+	{
+		if ((n % 3 == 0) && (n % 5 == 0))
+		{
+			printf("FizzBuzz");
+		}
+		else if (n % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (n % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", n);
+		}
 
-        if (n != 100)
-            printf(" ");
-    }
+		if (n != 100)
+			printf(" ");
+	}
 
-    printf("\n");
-    return (0);
+	printf("\n");
+	return (0);
 }
