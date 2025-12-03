1b3095233b47998a255d1d4701471c06513f63c8
javid802 <gccavid123@gmail.com>
2025-12-03 12:48:26 +0400

    python

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -1,31 +1,27 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * print_diagonal - draws a diagonal line on the terminal
+ * @n: number of times the character \ should be printed
  */
 void print_diagonal(int n)
 {
-    int a = 0;
-    int b = 0;
+	int a, b;
 
-    if (n>0)
-    {
-        for (a=0;a<n;a++)
-        {
-            for (b=0;b<a;b++)
-            {
-                _putchar(' ');
-            }
-            _putchar('\\');
-            _putchar('\n');
-        }
-    }
-    else
-    {
-        _putchar('\n');
-    }
-    
-        
+	if (n > 0)
+	{
+		for (a = 0; a < n; a++)
+		{
+			for (b = 0; b < a; b++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
 }
