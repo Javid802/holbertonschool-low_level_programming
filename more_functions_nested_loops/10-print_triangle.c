6fc3ae852b54a409f201e957ea3da7d844844fbe
javid802 <gccavid123@gmail.com>
2025-12-03 14:55:39 +0400

    python

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -1,25 +1,32 @@
 #include "main.h"
 
 /**
- * print_square - prints a square using the # character
- * @n: size of the square
+ * print_triangle - prints a right-aligned triangle using #
+ * @size: size of the triangle
  */
 void print_triangle(int size)
 {
     int count = 1;
     int spacecount = 1;
     int a;
-    for (count = 1;count<=size;count++)
-    {   
-        
-        for (spacecount=1;size-count>=spacecount;spacecount++)
-        {
-            printf(" ");
+
+    if (size > 0)
+    {
+        for (count = 1; count <= size; count++)
+        {   
+            for (spacecount = 1; size - count >= spacecount; spacecount++)
+            {
+                _putchar(' ');
+            }
+            for (a = 1; a <= count; a++)
+            {
+                _putchar('#');
+            }
+            _putchar('\n');
         }
-        for (a=1;a<=count;a++)
-        {
-            printf("#");
-        }
-        printf("\n");
+    }
+    else
+    {
+        _putchar('\n');
     }
 }
