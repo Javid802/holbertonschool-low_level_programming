5cdf53c7eea349126177a7c906bec990557a1305
javid802 <gccavid123@gmail.com>
2025-12-03 14:58:28 +0400

    python

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -6,27 +6,23 @@
  */
 void print_triangle(int size)
 {
-    int count = 1;
-    int spacecount = 1;
-    int a;
+	int count = 1;
+	int spacecount = 1;
+	int a;
 
-    if (size > 0)
-    {
-        for (count = 1; count <= size; count++)
-        {   
-            for (spacecount = 1; size - count >= spacecount; spacecount++)
-            {
-                _putchar(' ');
-            }
-            for (a = 1; a <= count; a++)
-            {
-                _putchar('#');
-            }
-            _putchar('\n');
-        }
-    }
-    else
-    {
-        _putchar('\n');
-    }
+	if (size > 0)
+	{
+		for (count = 1; count <= size; count++)
+		{	
+			for (spacecount = 1; size - count >= spacecount; spacecount++)
+				_putchar(' ');
+			for (a = 1; a <= count; a++)
+				_putchar('#');
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
 }
