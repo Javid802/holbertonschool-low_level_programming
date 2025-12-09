349bf312dc7075779f3b743995e81ff9418e34a5
javid802 <gccavid123@gmail.com>
2025-12-09 17:14:07 +0400

    python

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the values of two integers
+ * @a: pointer to first int
+ * @b: pointer to second int
+ */
+int _strlen(char *s)
+{
+    int a = 0;
+    while (s[a]!='\0')
+    {
+        a++;
+    }
+    return(a);
+}
