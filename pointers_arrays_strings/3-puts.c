c0d9d026a79e3569892a46fb5d3379c502fc29e5
javid802 <gccavid123@gmail.com>
2025-12-09 17:17:52 +0400

    python

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: pointer to the string
+ *
+ * Return: length of the string
+ */
+void _puts(char *str)
+{
+	int a = 0;
+
+	while (str[a] != '\0')
+	{
+		_putchar(str[a]);
+        a++;
+	}
+
+	return (a);
+}
