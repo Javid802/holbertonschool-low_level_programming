5a4e29d6aa0398b3e694881a528118acc8451214
javid802 <gccavid123@gmail.com>
2025-12-09 17:44:09 +0400

    python

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string
+ * @str: pointer to the string
+ */
+void puts_half(char *str)
+{
+	int a = 0;
+    int i = 0;
+
+	while (str[a] != '\0')
+	{
+		a++;
+	}
+    
+    while(i<a/2)
+    {
+        _putchar(str[i]);
+        i++;
+    }
+
+	_putchar('\n');
+}
