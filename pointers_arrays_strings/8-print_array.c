e003d38a435493a69c6229526ea30ed6708ff03b
javid802 <gccavid123@gmail.com>
2025-12-09 17:55:52 +0400

    python

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts_half - prints the second half of a string
+ * @str: pointer to the string
+ */
+void print_array(int *a, int n)
+{
+    int i;
+    for (i=0;i<n;i++)
+    {
+        printf("%d",a[i]);
+
+        if (i != n-1)
+        {
+            printf(", ");
+
+        }
+    printf("\n");
+    }
+}
