d40814a8b1675a29406fb83a7433ad4323fa4ceb
javid802 <gccavid123@gmail.com>
2025-12-09 17:36:30 +0400

    python

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse followed by a new line
+ * @s: pointer to the string
+ */
+void rev_string(char *s)
+{
+	int a = 0;
+    int b = 0;
+    char temporary;
+	int i;
+    
+
+	while (s[a] != '\0')
+	{
+		a++;
+	}
+    a--;
+    while (b<a)
+    {
+        temporary = s[b];
+        s[b] = s[a];
+        s[a] = temporary;
+        b ++;
+        a--;
+
+    }
+
+}
