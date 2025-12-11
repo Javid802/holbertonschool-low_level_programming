f28fc9133a09b6b78be33db57546e19b4e3e60ba
javid802 <gccavid123@gmail.com>
2025-12-11 13:26:06 +0400

    python

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -3,38 +3,45 @@
 #include <limits.h>
 
 /**
- * _atoi - convert string to int safely
- * @s: string
+ * _atoi - converts a string to an integer safely
+ * @s: string to convert
  *
- * Return: converted integer
+ * Return: the converted integer
  */
 int _atoi(char *s)
 {
     int sign = 1, found_digit = 0;
-    int64_t num = 0;  /* daha geniş tip ilə işləyirik */
+    int64_t num = 0;
 
     while (*s)
     {
         if (*s == '-' && !found_digit)
+        {
             sign = -sign;
+        }
         else if (*s >= '0' && *s <= '9')
         {
             int digit = *s - '0';
             found_digit = 1;
 
-            num = num * 10 + digit;  /* int64_t olduğu üçün overflow yoxdur */
+            num = num * 10 + digit;
         }
         else if (found_digit)
+        {
             break;
+        }
 
         s++;
     }
 
-    /* INT_MIN / INT_MAX ilə uyğunlaşdır */
     if (sign == 1 && num > INT_MAX)
-        return INT_MAX;
+    {
+        return (INT_MAX);
+    }
     else if (sign == -1 && -num < INT_MIN)
-        return INT_MIN;
+    {
+        return (INT_MIN);
+    }
 
-    return (int)(sign * num);
+    return ((int)(sign * num));
 }
