02f2cf6962036a653880e180148491e4c0cab2bb
javid802 <gccavid123@gmail.com>
2025-12-11 13:22:08 +0400

    python

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -2,33 +2,44 @@
 
 /**
  * _atoi - convert a string to an integer
- * @s: string to convert
+ * @s: string
  *
- * Return: converted integer
+ * Return: the integer
  */
 int _atoi(char *s)
 {
-    int sign = 1;
-    int num = 0;
-    int found_digit = 0;
+    int sign = 1, num = 0, found_digit = 0;
 
     while (*s != '\0')
     {
-        if (*s == '-' && found_digit == 0)
-        {
-            sign = sign * -1;
-        }
+        if (*s == '-' && !found_digit)
+            sign = -sign;
+
         else if (*s >= '0' && *s <= '9')
         {
+            int digit = *s - '0';
+
             found_digit = 1;
-            num = num * 10 + (*s - '0');
+
+            /* Overflow check BEFORE multiplying by 10 */
+            if (sign == 1)
+            {
+                if (num > (2147483647 - digit) / 10)
+                    return 2147483647;
+            }
+            else
+            {
+                if (num > (2147483648u - digit) / 10)
+                    return -2147483648;
+            }
+
+            num = num * 10 + digit;
         }
         else if (found_digit)
-        {
             break;
-        }
+
         s++;
     }
 
-    return (sign * num);
+    return num * sign;
 }
