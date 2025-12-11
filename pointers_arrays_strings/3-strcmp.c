fcc23d9758c8b51e8d45c4d46426f4d1a8d494a2
javid802 <gccavid123@gmail.com>
2025-12-11 18:40:40 +0400

    python

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: pointer to the string
+ *
+ * Return: length of the string
+ */
+int _strcmp(char *s1, char *s2)
+{
+	
+    while(*s1 && *s2 && s1==s2)
+    {
+        s1++;
+        s2++;
+    }
+    return(*s1-*s2)
+}
