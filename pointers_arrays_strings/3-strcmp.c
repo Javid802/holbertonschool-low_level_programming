f27dba6ba1a15c91a0944599d0126671afaca397
javid802 <gccavid123@gmail.com>
2025-12-13 01:01:35 +0400

    python

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,18 +1,22 @@
 #include "main.h"
 
 /**
- * _strlen - returns the length of a string
- * @s: pointer to the string
+ * _strcmp - compares two strings
+ * @s1: first string
+ * @s2: second string
  *
- * Return: length of the string
+ * Return: negative, zero, or positive value
  */
 int _strcmp(char *s1, char *s2)
 {
-	
-    while(*s1 && *s2 && s1==s2)
-    {
-        s1++;
-        s2++;
-    }
-    return(*s1-*s2);
+	int i = 0;
+
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
+	}
+
+	return (s1[i] - s2[i]);
 }
