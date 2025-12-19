9277540efafacde9331c76989a977e8574207276
javid802 <gccavid123@gmail.com>
2025-12-19 16:59:34 +0400

    python

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -1,21 +1,19 @@
 #include "main.h"
 
 /**
- * factorial - returns the factorial of a number
- * @n: the number
+ * _pow_recursion - returns the value of x raised to the power of y
+ * @x: base
+ * @y: exponent
  *
- * Return: factorial of n, -1 if n < 0
+ * Return: x^y, -1 if y < 0
  */
 int _pow_recursion(int x, int y)
 {
-	if (y<0)
-    {
-        return(-1);
-    }
-    if(y==0)
-    {
-        return(1);
-    }
+	if (y < 0)
+		return (-1);
 
-    return(x*_pow_recursion(x,y-1));
+	if (y == 0)
+		return (1);
+
+	return (x * _pow_recursion(x, y - 1));
 }
