bd0490aa465e88ee6e1996bd9330630ebc4dda97
javid802 <gccavid123@gmail.com>
2025-12-19 16:58:28 +0400

    python

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -16,3 +16,4 @@ int factorial(int n)
 
 	return (n * factorial(n - 1));
 }
+
---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * factorial - returns the factorial of a number
+ * @n: the number
+ *
+ * Return: factorial of n, -1 if n < 0
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y<0)
+    {
+        return(-1);
+    }
+    if(y==0)
+    {
+        return(0);
+    }
+
+    return(x*_pow_recursion(x,y-1));
+}
