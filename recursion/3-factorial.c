9128fead2f4b53e24a6c019619421668b00e5f25
javid802 <gccavid123@gmail.com>
2025-12-19 16:49:11 +0400

    python

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -1,21 +1,18 @@
 #include "main.h"
 
 /**
- * _strlen_recursion - returns the length of a string
- * @s: pointer to string
+ * factorial - returns the factorial of a number
+ * @n: the number
  *
- * Return: length of the string
+ * Return: factorial of n, -1 if n < 0
  */
 int factorial(int n)
 {
-	if (n<0)
-    {
-        return(-1);
-    }
-    else if(n==0)
-    {   
-        return(1);
-    }
-    return(n * factorial(n-1));
+	if (n < 0)
+		return (-1);
 
+	if (n == 0)
+		return (1);
+
+	return (n * factorial(n - 1));
 }
