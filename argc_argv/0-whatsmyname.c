fcdf4e950b2a4fe3642bf49e106cbdcbedbfb979
javid802 <gccavid123@gmail.com>
2025-12-19 18:44:57 +0400

    python

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 /**
- * main - proqramın adını çap edir
- * @argc: arqumentlərin sayı
- * @argv: arqumentlərin siyahısı
+ * main - prints the program name
+ * @argc: argument count
+ * @argv: argument vector
  *
  * Return: 0
  */
 int main(int argc, char *argv[])
 {
-    (void)argc;      /* xəbərdarlığı aradan qaldırır */
-    printf("%s\n", argv[0]);
-    return 0;
+(void)argc; /* suppress unused parameter warning */
+printf("%s\n", argv[0]);
+return (0);
 }
