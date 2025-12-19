28f88da94e2650855f3b726ef02ad07081716857
javid802 <gccavid123@gmail.com>
2025-12-19 16:17:20 +0400

    python

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -2,16 +2,14 @@
 #include "main.h"
 
 /**
- * _puts_recursion - prints a string, followed by a new line
+ * _print_rev_recursion - prints a string in reverse
  * @s: pointer to string
  */
 void _print_rev_recursion(char *s)
 {
-	if (*s == '\0')
-	{
-		return;
-	}
+    if (*s == '\0')
+        return;
 
     _print_rev_recursion(s + 1);
-	write(1, s, 1);
+    write(1, s, 1);
 }
