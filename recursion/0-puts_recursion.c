6d51ce6e7523acf401097b2f2abaaeb3d133f431
javid802 <gccavid123@gmail.com>
2025-12-19 15:42:30 +0400

    python

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,23 +1,18 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
 #include <unistd.h>
 #include "main.h"
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * _puts_recursion - prints a string, followed by a new line
+ * @s: pointer to string
  */
-
 void _puts_recursion(char *s)
 {
-    if(*s=='\0')
-    {
-        write(1,"\n",1);
-        return;
-    }
-    write(1,s,1);
-    _puts_recursion(s + 1);
+	if (*s == '\0')
+	{
+		write(1, "\n", 1);
+		return;
+	}
 
+	write(1, s, 1);
+	_puts_recursion(s + 1);
 }
