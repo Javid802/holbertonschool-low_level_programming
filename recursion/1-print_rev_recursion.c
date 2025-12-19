2b6c28b1067d7a1b49b68d06c1b37603ea58c0ba
javid802 <gccavid123@gmail.com>
2025-12-19 16:17:45 +0400

    python

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -7,9 +7,9 @@
  */
 void _print_rev_recursion(char *s)
 {
-    if (*s == '\0')
-        return;
+	if (*s == '\0')
+		return;
 
-    _print_rev_recursion(s + 1);
-    write(1, s, 1);
+	_print_rev_recursion(s + 1);
+	write(1, s, 1);
 }
